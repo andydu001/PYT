41185a150476ed44e83a40e182c14b90c107d7eb
Andy Duverneau <77910275+andydu001@users.noreply.github.com>
2025-03-14 23:46:13 -0400

    Add files via upload

---
+++ MACH02/src/main/java/org/example/PY0.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.python.core.Py;
+
+import org.python.modules._io.OpenMode;
+
+import org.python.modules._io.PyFileIO;
+
+public class PY0 extends PyFileIO{
+    PY0() {
+        super(Py.newString("C:\\Users\\andyd\\OneDrive\\Documents\\place101.txt"), new OpenMode(readall_doc), true);
+
+        Py.writeMessage("error", " writing");
+
+        Py.println(Py.newFloat(2.0));
+
+        Py.println(Py.newString("I cant write enough"));
+
+        String string  = Py.fileSystemDecode(Py.newString("C:\\Users\\andyd\\OneDrive\\Documents\\place101.txt"));
+
+        System.out.println(string);
+
+        //System.out.println(__len__());
+
+    }
+}
